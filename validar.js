cd2b283da6e8e06998af4851074fd2bd74519a48
Brian Hazel <briraptor21@gmail.com>
2024-09-11 20:19:28 -0600

    validando los campos

--- validar.js
+++ validar.js
@@ -1,9 +1,18 @@
 function validar(form) {
 let nombre = form.nombre.value.trim();
+let edad = form.edad.value.trim();
+let sexo = form.sexo.value.trim();
 if (nombre === "") {
     alert("El campo nombre es obligatorio.");
         return false; // Detiene el envío del formulario
-       console.log("El campo nombre es obligatorio.");
+}
+if (edad === "") {
+    alert("El campo edad es obligatorio.");
+        return false; // Detiene el envío del formulario
+}
+if (sexo === "") {
+    alert("El campo sexo es obligatorio.");
+        return false; // Detiene el envío del formulario
 }
 return true; // Permite el envío del formulario
 }
